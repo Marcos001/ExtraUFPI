9a36792dd6f16d7aeb742d262a76b5f74a8dbb22
Marcos001 <santosMsantos01@gmail.com>
2018-02-10 16:46:10 -0300

    first commit

---
+++ cabecalho.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Extra UFPI</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/loja.css">
+</head>
+<body>
+
+<div class="navbar navbar-inverse navbar-fixed-top">
+
+	<div class="container"> <!-- container -->
+		
+	<div class="navbar-header">
+		<a class="navbar-brand" href="index.php"> EXTRA UFPI LOGO </a>
+	</div>
+
+
+	</div> <!-- container -->
+
+	
+</div>
+
+<div class="container">
+	<div class="principal">
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include("cabecalho.php"); ?>
+
+
+<table class="table">
+    
+    <tr>
+        <td class="form-control"><h1>Campus UFPI</h1></td>
+    </tr>
+    
+	<tr>
+        <td><a  href="#"><button type="button" class="btn btn-primary form-control">Bom Jesus</button> </a></td>
+	<tr>
+        
+    <tr>
+       <td> <a  href="#"><button type="button" class="btn btn-primary form-control">Parnaíba</button> </a></td>
+	<tr>
+        
+    <tr>
+       <td> <a  href="campus/picos/index.php"><button type="button" class="btn btn-primary form-control">Picos</button> </a></td>
+	<tr>
+        
+    <tr>
+        <td><a  href="#"><button type="button" class="btn btn-primary form-control">Teresina</button> </a></td>	
+	</tr>
+
+		
+</table>
+
+
+        
+<?php include("rodape.php"); ?>
